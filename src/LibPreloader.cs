9a28cd928424c357ab2ab0a71dc57d2356ff57c8
LazuliKao <lazulikao@nbt.edu.cn>
2023-07-22 16:19:26 +0800

    opti: Hook Api

--- src/LibPreloader.cs
+++ src/LibPreloader.cs
@@ -6,6 +6,20 @@ internal static class LibPreloader
 {
     const string libname = "libpreloader";
 
-    [DllImport(libname)]
-    internal static extern unsafe int hook(nint rva, void* hook, out void* org);
+    [DllImport(libname, EntryPoint = "hook", ExactSpelling = true)]
+    internal static extern unsafe int Hook(void* rva, void* hook, out void* org);
+
+    internal static int Hook(nint rva, nint hook, out nint org)
+    {
+        if (rva == 0)
+            throw new NullReferenceException(nameof(rva));
+        if (hook == 0)
+            throw new NullReferenceException(nameof(hook));
+        unsafe
+        {
+            var result = Hook((void*)rva, (void*)hook, out var orgPtr);
+            org = (nint)orgPtr;
+            return result;
+        }
+    }
 }
