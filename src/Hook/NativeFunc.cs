ba36cd975a1943e48c22b8f6ca9533e80262948f
LazuliKao <lazulikao@nbt.edu.cn>
2023-07-22 22:39:59 +0800

    feat: unhook

--- src/Hook/NativeFunc.cs
+++ src/Hook/NativeFunc.cs
@@ -4,16 +4,80 @@ namespace NativeInterop.Hook;
 
 public class NativeFunc
 {
+    public class HookInstance
+    {
+        internal unsafe HookInstance(FuncHookT* instance, void* original)
+        {
+            _instance = instance;
+            _original = original;
+        }
+
+        private unsafe FuncHookT* _instance;
+        private unsafe void* _original;
+        internal unsafe FuncHookT* Instance
+        {
+            get
+            {
+                CheckActive();
+                return _instance;
+            }
+        }
+        public unsafe void* Original
+        {
+            get
+            {
+                CheckActive();
+                return _original;
+            }
+        }
+
+        public HookResult Uninstall()
+        {
+            unsafe
+            {
+                CheckActive();
+                var result = LibHook.Unhook(ref _original, ref _instance);
+                if (result == HookResult.Success)
+                {
+                    _instance = null;
+                    _original = null;
+                }
+                return result;
+            }
+        }
+
+        private void CheckActive()
+        {
+            unsafe
+            {
+                if (_original is null || _instance is null)
+                {
+                    throw new InvalidOperationException(
+                        "This hook is not active. Maybe already uninstalled."
+                    );
+                }
+            }
+        }
+    }
+
     /// <summary>
     /// Hook a function by its address.
     /// </summary>
     /// <param name="address">The address of the function.</param>
     /// <param name="hook">The hook function you created.</param>
     /// <param name="org">The original function.</param>
+    /// <param name="instance"> The instance of current hook </param>
     /// <returns>hook result (0 if succeed)</returns>
-    public static unsafe int Hook(void* address, void* hook, out void* org)
+    public static unsafe HookResult Hook(
+        void* address,
+        void* hook,
+        out void* org,
+        out HookInstance instance
+    )
     {
-        return LibHook.Hook(address, hook, out org);
+        var result = LibHook.Hook(address, hook, out org, out var t);
+        instance = new HookInstance(t, org);
+        return result;
     }
 
     /// <summary>
@@ -22,10 +86,21 @@ public class NativeFunc
     /// <param name="offset">The offset of the function.</param>
     /// <param name="hook">The hook function you created.</param>
     /// <param name="org">The original function.</param>
+    /// <param name="instance"></param>
     /// <returns>hook result (0 if succeed)</returns>
-    public static unsafe int Hook(int offset, void* hook, out void* org)
+    internal static unsafe HookResult Hook(
+        int offset,
+        void* hook,
+        out void* org,
+        out HookInstance instance
+    )
     {
-        return LibHook.Hook((HandleHelper.MainHandleHandle + offset).ToPointer(), hook, out org);
+        return Hook(
+            (HandleHelper.MainHandleHandle + offset).ToPointer(),
+            hook,
+            out org,
+            out instance
+        );
     }
 
     /// <summary>
@@ -36,7 +111,7 @@ public class NativeFunc
     /// <param name="org">The original function.</param>
     /// <returns>hook result (0 if succeed)</returns>
 
-    public static int Hook(nint address, nint hook, out nint org)
+    public static HookResult Hook(nint address, nint hook, out nint org, out HookInstance instance)
     {
         if (address == 0)
             throw new NullReferenceException(nameof(address));
@@ -44,7 +119,7 @@ public class NativeFunc
             throw new NullReferenceException(nameof(hook));
         unsafe
         {
-            var result = LibHook.Hook((void*)address, (void*)hook, out var orgPtr);
+            var result = Hook((void*)address, (void*)hook, out var orgPtr, out instance);
             org = (nint)orgPtr;
             return result;
         }
@@ -58,8 +133,8 @@ public class NativeFunc
     /// <param name="org">The original function.</param>
     /// <returns>hook result (0 if succeed)</returns>
 
-    public static int Hook(int offset, nint hook, out nint org)
+    public static HookResult Hook(int offset, nint hook, out nint org, out HookInstance instance)
     {
-        return Hook(HandleHelper.MainHandleHandle + offset, hook, out org);
+        return Hook(HandleHelper.MainHandleHandle + offset, hook, out org, out instance);
     }
 }
--- src/LibLoader/LibHook.cs
+++ src/LibLoader/LibHook.cs
@@ -2,10 +2,46 @@
 
 namespace NativeInterop.LibLoader;
 
+public enum HookResult : int
+{
+    InternalError = -1,
+    Success = 0,
+    ErrorOutOfMemory = 1,
+    ErrorAlreadyInstalled = 2,
+    ErrorDisassembly = 3,
+    ErrorIpRelativeOffset = 4,
+    ErrorCannotFixIpRelative = 5,
+    ErrorFoundBackJump = 6,
+    ErrorTooShortInstructions = 7,
+
+    /// <summary>
+    /// memory allocation error
+    /// </summary>
+    ErrorMemoryAllocation = 8,
+
+    /// <summary>
+    ///  other memory function errors
+    /// </summary>
+    ErrorMemoryFunction = 9,
+    ErrorNotInstalled = 10,
+    ErrorNoAvailableRegisters = 11,
+    ErrorNoSpaceNearTargetAddr = 12,
+}
+
+internal struct FuncHookT { }
+
 internal static class LibHook
 {
-    const string libname = "libhook";
+    const string LibName = "libhook";
 
-    [DllImport(libname, EntryPoint = "hook", ExactSpelling = true)]
-    internal static extern unsafe int Hook(void* rva, void* hook, out void* org);
+    [DllImport(LibName, EntryPoint = "hook", ExactSpelling = true)]
+    internal static extern unsafe HookResult Hook(
+        void* rva,
+        void* hook,
+        out void* org,
+        out FuncHookT* instance
+    );
+
+    [DllImport(LibName, EntryPoint = "unhook", ExactSpelling = true)]
+    internal static extern unsafe HookResult Unhook(ref void* org, ref FuncHookT* instance);
 }
