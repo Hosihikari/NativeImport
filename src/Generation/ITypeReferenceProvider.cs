04aab72c6c167ba23cb4e2e258e908aaaddbf9a1
Pd <minecraftpd233@163.com>
2023-10-26 20:03:12 +0800

    feat: Add ITypeReferenceProvider

---
+++ src/Generation/ITypeReferenceProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Hosihikari.NativeInterop.Generation;
+
+public interface ITypeReferenceProvider
+{
+    public static abstract Regex Regex { get; }
+
+    public static abstract Type Matched(Regex regex);
+}
--- src/Generation/PredefinedTypeAttribute.cs
+++ src/Generation/PredefinedTypeAttribute.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Hosihikari.NativeInterop.Generation;
+﻿namespace Hosihikari.NativeInterop.Generation;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
 public class PredefinedTypeAttribute : Attribute
 {
+    public string NativeTypeName { get; set; } = string.Empty;
+
+    public string NativeTypeNamespace { get; set; } = string.Empty;
 }
